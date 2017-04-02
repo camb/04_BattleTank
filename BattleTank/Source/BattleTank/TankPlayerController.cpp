358b284411a0579cba15a3decae882635bbfb727
camb <cameron.bec@gmail.com>
2017-04-02 14:51:33 -0700

    4.120 setup tank controller Tick() and AimTowardsCrosshair()

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,18 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+
+// Called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//AimTowardsCrosshair();
+	UE_LOG(LogTemp, Warning, TEXT("Ticking ATankPlayerController!"));
+
+}
+
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -23,4 +35,13 @@ void ATankPlayerController::BeginPlay()
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// Get world location through crosshair
+	// If it hits the landscape
+		// Tell controlled tank to aim at point
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -18,6 +18,11 @@ public:
 	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
-	
-	
+
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// Start moving the tank barrel to crosshair
+	void ATankPlayerController::AimTowardsCrosshair();
+
 };

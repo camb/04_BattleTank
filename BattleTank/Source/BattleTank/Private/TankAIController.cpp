1e4e755ca2e56a0331f581072fd0ca4bd0488a6a
camb <cameron.bec@gmail.com>
2017-04-02 13:22:47 -0700

    4.119 implemented GetPlayerTank() for AI tank class

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -7,14 +7,14 @@ void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController not possessing a tank."));
+		UE_LOG(LogTemp, Warning, TEXT("AI couldn't find player tank!"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController possessing: %s"), *(ControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("Found player tank: %s"), *(PlayerTank->GetName()));
 	}
 }
 
@@ -22,4 +22,13 @@ void ATankAIController::BeginPlay()
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+
+	return Cast<ATank>(PlayerPawn);
 }
